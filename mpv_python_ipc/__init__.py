54e89b7c31b7e403547dcf5d9faf1173fdefeb7c
siikamiika <siikamiika@users.noreply.github.com>
2015-10-03 17:30:15 +0300

    fix install

---
+++ mpv_python_ipc/__init__.py
@@ -0,0 +1,5 @@
+from .mpv_python_ipc import MpvProcess
+
+__all__ = ["MpvProcess"]
+
+__version__ = "1.0"
--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@
 
 from distutils.core import setup
 
-setup(name='mpv-python-ipc',
+setup(name='mpv_python_ipc',
     version='1.0',
     description='Python bindings to mpv lua api',
     author='siikamiika',
     url='https://github.com/siikamiika/mpv-python-ipc',
-    packages=['mpv-python-ipc'],
-    package_data={'mpv-python-ipc': ['ipc.lua']},
-    package_dir={'mpv-python-ipc': '.'}
+    packages=['mpv_python_ipc'],
+    package_data={'mpv_python_ipc': ['ipc.lua']},
 )
